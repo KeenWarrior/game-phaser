ce8ea353513c1dddbaf0781bbd46da23e2efd81d
KeenWarrior <anujgargcse@gmail.com>
2018-12-28 19:34:45 +0530

    first commit

---
+++ L1/website/data.js
@@ -0,0 +1,35 @@
+nums = ["happy", 0, 2, 5, 7, 9]
+
+for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+    console.log(element);
+}
+
+function sq(x){
+    return x * x;
+}
+
+lam = (x) => ( x * x )
+
+console.info(lam(4))
+
+naya = nums.filter(item => {return item})
+
+console.log(naya);
+
+nums.forEach(element => {
+    console.log(element);
+});
+
+a = {
+    dance : function(){
+        console.log("Nacho");
+    },
+
+    arr : [34, 78, 67],
+
+    name : "Anuj ka object"
+}
+
+console.log(a);
+
